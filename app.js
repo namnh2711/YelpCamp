d285720b476128580c935c1c2c68106d7f0cc2d8
nam3t <namnh2711@gmail.com>
2017-11-11 00:41:36 +0700

    Add database

--- app.js
+++ app.js
@@ -1,46 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
+// native ES6 promises
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/yelp_camp', {
+    useMongoClient: true
+})
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+const campgroundSchema = mongoose.Schema({
+    name: String,
+    src: String,
+    description: String
+})
+
+const Campground = mongoose.model('Campground', campgroundSchema)
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('landing-page');
-});
-let campgrounds = [
-    {name: '1', src: 'https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '2', src: 'https://images.unsplash.com/photo-1428447207228-b396f310848b?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '3', src: 'https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '1', src: 'https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '2', src: 'https://images.unsplash.com/photo-1428447207228-b396f310848b?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '3', src: 'https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '1', src: 'https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '2', src: 'https://images.unsplash.com/photo-1428447207228-b396f310848b?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '3', src: 'https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '1', src: 'https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '2', src: 'https://images.unsplash.com/photo-1428447207228-b396f310848b?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-    {name: '3', src: 'https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?dpr=1&auto=compress,format&fit=crop&w=1050&h=&q=80&cs=tinysrgb&crop='},
-];
+    res.render('landing-page')
+})
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', { campgrounds });
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('index', { campgrounds })
+        }
+    })
 })
 
 app.post('/campgrounds', (req, res) => {
-    const name = req.body.name;
-    const image = req.body.image;
-    const newCampground = {name, src: image};
-    campgrounds.push(newCampground);
+    const name = req.body.name
+    const image = req.body.image
+    const description = req.body.description
+    const newCampground = {name, src: image, description}
+    Campground.create(newCampground, (err, newCreated) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/campgrounds')
+        }
+    })
 
-    res.redirect('/campgrounds');
 })
 
 app.get('/campgrounds/new', (req, res) => {
-    res.render('new');
+    res.render('new')
+})
+
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id, (err, foundOne) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('show', {camp: foundOne})
+        }
+    })
 })
 
 app.listen(8080, '127.0.0.1', () => {
-    console.log('Server is running...');
+    console.log('Server is running...')
 })
