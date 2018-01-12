7706fb071d002d838f1e2a93f5302c7db74fd495
Nam Nguyen Hoang <nhnam2@cmc.com.vn>
2018-01-12 14:12:10 +0700

    add condition show/hide current user

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
+
 const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
